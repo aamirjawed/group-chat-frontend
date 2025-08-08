2c1db178b5fcc7ed95e60eaaa26d3513dd580bef
aamirjawed <aamirjawed682@gmail.com>
2025-08-08 06:36:24 +0530

    some changes in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Security-Policy': "connect-src 'self' http://localhost:5000 https://group-chat-backend-s7xa.onrender.com"
+    }
+  },
+  preview: {
+    headers: {
+      'Content-Security-Policy': "connect-src 'self' http://localhost:5000 https://group-chat-backend-s7xa.onrender.com"
+    }
+  }
 })
